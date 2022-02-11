18e76389163c537721454dd469f5d785a082bbbb
Driss <driss.dc9@gmail.com>
2022-02-11 12:32:46 +0100

    first commit

---
+++ LocationBDD/Client.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace LocationBDD
+{
+    public class Client
+    {
+        public string Username { get; private set; }
+
+        public string Password { get; private set; }
+
+        public Client(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+    }
+}
---
+++ LocationSpecFlow/Fake/FakeDataLayer.cs
@@ -0,0 +1,17 @@
+﻿using LocationBDD;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocationSpecFlow.Fake
+{
+    class FakeDataLayer : IDataLayer
+    {
+        public List<Client> Clients { get; set; }
+
+        public FakeDataLayer()
+        {
+            this.Clients = new List<Client>();
+        }
+    }
+}
