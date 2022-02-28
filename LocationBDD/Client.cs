2a8e5361036abf546292b4a94f23227a07b35f02
axel aubry <axel.aubry69@gmail.com>
2022-02-28 11:31:58 +0100

    WIP : register en cour

--- LocationBDD/Client.cs
+++ LocationBDD/Client.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 
 namespace LocationBDD
 {
@@ -8,10 +8,27 @@ namespace LocationBDD
 
         public string Password { get; private set; }
 
+        public string Name { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public DateTime Birthday { get; private set; }
+
+        public DateTime LicenceDate { get; private set; }
+
         public Client(string username, string password)
         {
             this.Username = username;
             this.Password = password;
+        }
+
+        public Client(string username, string password, string name, string lastName, DateTime licenceDate)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Name = name;
+            this.LastName = lastName;
+            this.Birthday = licenceDate;
         }
     }
 }
